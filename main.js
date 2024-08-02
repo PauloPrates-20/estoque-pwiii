9d8209a13a646b91a7229afb264a7bff3e65c5b0
Paulo Prates <paulo.prates01@hotmail.com>
2024-08-01 22:26:27 -0300

    Create update funcion

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 // Form inputs
+const inputId = document.getElementById('id');
 const inputProduto = document.getElementById('produto');
 const inputPreco = document.getElementById('preco');
 const inputQuantidade = document.getElementById('qtd');
 
-// Submit button
+// Button containers
+const submitContainer = document.getElementById('submit-container');
+const editContainer = document.getElementById('edit-container')
+
+// Buttons
 const submitButton = document.getElementById('submit');
+const confirmButton = document.getElementById('confirmar');
+const cancelButton = document.getElementById('cancelar');
 
 // Product list
 const productList = document.getElementById('lista-produtos');
@@ -41,6 +48,17 @@ function getProducts() {
                 element.addEventListener('click', function () {
                     const id = parseInt(this.getAttribute('id').replace('del', ''));
                     deleteProduct({ id: id });
+                });
+            }
+
+            // Edit event listener
+            for (const element of document.getElementsByClassName('edit')) {
+                element.addEventListener('click', function() {
+                    const id = parseInt(this.getAttribute('id').replace('edit', ''));
+                    inputId.value = id;
+
+                    submitContainer.classList.add('hidden');
+                    editContainer.classList.remove('hidden');
                 });
             }
         }
@@ -83,6 +101,29 @@ function deleteProduct(id) {
 
 }
 
+// Update database
+function updateProduct(data) {
+    const request = new XMLHttpRequest();
+    request.open('POST', 'update.php', true);
+    request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(this.responseText);
+            getProducts();
+        }
+    }
+
+    request.send(JSON.stringify(data));
+}
+
+function limparEntradas() {
+    inputId.value = '';
+    inputProduto.value  = '';
+    inputPreco.value = '';
+    inputQuantidade.value = '';
+}
+
 /* Event Listeners */
 
 // Submit event listener
@@ -95,9 +136,32 @@ submitButton.addEventListener('click', () => {
 
     insertProduct(data);
 
-    inputProduto.value  = '';
-    inputPreco.value = '';
-    inputQuantidade.value = '';
+    limparEntradas();
+});
+
+// Confirm event listener
+confirmButton.addEventListener('click', () => {
+    const id = inputId.value;
+    const produto = inputProduto.value;
+    const preco = parseFloat(inputPreco.value);
+    const qtd = parseInt(inputQuantidade.value);
+
+    const data = { id: id, produto: produto, preco: preco, qtd: qtd };
+
+    updateProduct(data);
+
+    limparEntradas();
+
+    submitContainer.classList.remove('hidden');
+    editContainer.classList.add('hidden');
+});
+
+// Cancel event listener
+cancelButton.addEventListener('click', () => {
+    limparEntradas();
+
+    submitContainer.classList.remove('hidden');
+    editContainer.classList.add('hidden');
 });
 
 // Initial load
